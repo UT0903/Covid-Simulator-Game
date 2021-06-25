07cff8aefe770fef4f78fda05f5e101f24842394
imingasco <imingasco123@yahoo.com.tw>
2021-06-25 16:08:58 +0800

    Setup Panels

--- src/GameFlow.java
+++ src/GameFlow.java
@@ -8,7 +8,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class GameFlow {
-    private JPanel mapPanel, toolbarPanel;
+    private JPanel mapPanel, toolbarPanel, messagePanel, infoPanel, detailPanel;
     private JFrame windowFrame;
     private static final String basePath = "../pic/";
     private static final String[] tools = {
@@ -20,18 +20,39 @@ public class GameFlow {
     public GameFlow(){
         mapPanel = new JPanel();
         toolbarPanel = new JPanel();
+        messagePanel = new JPanel();
+        infoPanel = new JPanel();
+        detailPanel = new JPanel();
         windowFrame = new JFrame();
     }
     public void start(){
+        windowFrame.getContentPane().setLayout(null);
         windowFrame.setSize(960, 640);
-        mapPanel.setSize(960, 640);
+        messagePanel.setSize(800, 40);
+        mapPanel.setSize(800, 600);
+        infoPanel.setSize(160, 100);
+        toolbarPanel.setSize(160, 400);
+        detailPanel.setSize(160, 140);
         mapPanel.setLayout(new BorderLayout());
         JLabel bgPic = new JLabel(new ImageIcon(basePath + "map.png"));
         mapPanel.add(bgPic);
 
+        // Adding color just for testing
+        messagePanel.setBackground(Color.green);
+        infoPanel.setBackground(Color.black);
+        toolbarPanel.setBackground(Color.yellow);
+        detailPanel.setBackground(Color.red);
 
-
+        windowFrame.add(messagePanel);
+        messagePanel.setLocation(0, 0);
+        windowFrame.add(infoPanel);
+        infoPanel.setLocation(800, 0);
         windowFrame.add(mapPanel);
+        mapPanel.setLocation(0, 40);
+        windowFrame.add(toolbarPanel);
+        toolbarPanel.setLocation(800, 100);
+        windowFrame.add(detailPanel);
+        detailPanel.setLocation(800, 500);
         windowFrame.setVisible(true);
         //map.setTitle("Virus Game");
         //map.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
