c05d47050fa9ff4073c365eac3cb940f305768dd
CloudWalkerW <willis.wu@funpodium.net>
2021-06-26 23:08:26 +0800

    can remove reddots

--- src/panels/MapPanel.java
+++ src/panels/MapPanel.java
@@ -17,30 +17,39 @@ import static panels.Utils.resizeImage;
 public class MapPanel extends JLayeredPane implements ActionListener {
     private static final int delay = 1000;
     private final Timer timer = new Timer(delay, this);
-
+    private ArrayList<JLabel> redDots = new ArrayList<JLabel>();
     public MapPanel() {
-        setPreferredSize(new Dimension(750, 550));
-        //setLocation(0, 40);
+        setPreferredSize(new Dimension(750, 600));
+//        setLocation(0, 40);
         JLabel bgPic = new JLabel(new ImageIcon(basePath + "./map.png")); //Add background
         bgPic.setOpaque(true);
-        bgPic.setSize(750, 550);
+        bgPic.setSize(750, 600);
         add(bgPic, Integer.valueOf(0));
     }
     public void start() {timer.start();}
     public void stop() { timer.stop(); }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int x = (int) (Math.random() * 730) + 10;
+        int y = (int) (Math.random() * 580) + 10;
+        addRedDot(x, y);
+    }
     private void addRedDot(int x , int y){
         ImageIcon redDotIcon = resizeImage(basePath + "./virus/1.png", 20, 20);
         JLabel redDot = new JLabel(redDotIcon);
         redDot.setSize(20,20);
         add(redDot, Integer.valueOf(1));
         redDot.setLocation(x,y);
+        redDots.add(redDot);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        int x = (int) (Math.random() * 750);
-        int y = (int) (Math.random() * 600);
-        addRedDot(x, y);
+    private void removeRedDots(ArrayList<JLabel> removeList){
+        for (int i = 0; i < removeList.size(); i++){
+            this.remove(removeList.get(i));
+        }
+        redDots.removeAll(removeList);
     }
 
 }
