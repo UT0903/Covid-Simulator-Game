256e6df8ca5fb779d8781bb8ae6b9fea09482a9d
imingasco <imingasco123@yahoo.com.tw>
2021-07-01 00:32:17 +0800

    modify percentage

--- src/Game/StateManager.java
+++ src/Game/StateManager.java
@@ -212,7 +212,6 @@ public class StateManager {
         updateGold(gold + goldChange);
     }
 
-
     public static final String[] areaNames = {
         "五股泰山",
         "三重蘆洲",
@@ -266,7 +265,8 @@ public class StateManager {
         0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2 ,0.2, 0.2, 0.2
     };
 
-    public synchronized static void setAmbulance(int amount, int groupID) {
+
+    public static void setAmbulance(int amount, int groupID) {
         itemInAreaNum[0][groupID] += amount;
         itemLastNum[0] -= amount;
         areaDeadProbability[groupID] = Math.max(0.01, areaDeadProbability[groupID] * Math.pow(0.95, amount));
@@ -274,7 +274,7 @@ public class StateManager {
             areaDeadProbability[neighbor] = Math.max(0.01, areaDeadProbability[neighbor] * Math.pow(0.97, amount));
     }
 
-    public synchronized static void setCanopy(int amount, int groupID) {
+    public static void setCanopy(int amount, int groupID) {
         itemInAreaNum[1][groupID] += amount;
         itemLastNum[1] -= amount;
         areaDeadProbability[groupID] = Math.max(0.01, areaDeadProbability[groupID] * Math.pow(0.8, amount));
@@ -297,7 +297,7 @@ public class StateManager {
         }
     }
 
-    public synchronized static void setHospital(int amount, int groupID) {
+    public static void setHospital(int amount, int groupID) {
         itemInAreaNum[2][groupID] += amount;
         itemLastNum[2] -= amount;
         areaDeadProbability[groupID] = Math.max(0.01, areaDeadProbability[groupID] * Math.pow(0.9, amount));
@@ -333,7 +333,7 @@ public class StateManager {
         }
     }
 
-    public synchronized static void setSyringe(int amount, int groupID) {
+    public static void setSyringe(int amount, int groupID) {
         itemInAreaNum[5][groupID] += amount;
         itemLastNum[5] -= amount;
         areaDeadProbability[groupID] = Math.max(0.01, areaDeadProbability[groupID] * Math.pow(0.95, amount));
@@ -341,12 +341,12 @@ public class StateManager {
         areaRecoverProbability[groupID] = Math.min(1.0, areaRecoverProbability[groupID] * Math.pow(1.05, amount));
     }
 
-    public synchronized static void updateAreaDeadProbability() {
+    public static void updateAreaDeadProbability() {
         for (int i = 0; i < 12; i++)
             areaDeadProbability[i] = Math.min(1.0, areaDeadProbability[i] * (1 + (double) areaPeopleInfectedNum[i] / (double) areaPeopleNum[i] / 2.0));
     }
 
-    public synchronized static void updateAreaPeopleDeadNum(){
+    public static void updateAreaPeopleDeadNum(){
         for (int i = 0; i < 12; i++) {
             double rand = Math.random() / 2.0 + 0.75; // 0.75 ~ 1.25
             double finalDeadProbability = Math.min(areaDeadProbability[i] * rand, 1.0);
@@ -375,16 +375,16 @@ public class StateManager {
     //Virus states management
     private static List<VirusListener> virusListeners = new ArrayList<>();
     public static void addVirusListener(VirusListener vl) { virusListeners.add(vl); }
-    public synchronized static int getAmount() {
+    public static int getAmount() {
         int a = 0;
         for (int j = 0; j < 12; j++){
             a += viruses.get(j).size();
         }
         return a;
     }
-    public synchronized static ArrayList<ArrayList<Virus>> getVirus() { return viruses; }
-    public synchronized static int getAreaPercentage(int index) { return (int) ((double) viruses.get(index).size() / (double) total[index]); }
-    public synchronized static int getPercentage() { return (int) ((double) getAmount() / 150 / 100 * 100); }
+    public static ArrayList<ArrayList<Virus>> getVirus() { return viruses; }
+    public static double getAreaPercentage(int index) { return ((double) viruses.get(index).size() / (double) total[index]); }
+    public static int getPercentage() { return (int) ((double) getAmount() / (150 * 120) * 100); }
 
     public static void spreadVirus(){
         List<Virus> spreadList = new ArrayList<>();
@@ -412,7 +412,7 @@ public class StateManager {
             vl.onVirusIncreased(spreadList);
     }
 
-    public synchronized static void addVirus(){
+    public static void addVirus(){
         int rand = (int) (Math.random() * 12);
         int i = (int) Math.round(Math.random() * notChosen.get(rand).size());
         areaPeopleInfectedNum[rand]++;
@@ -426,7 +426,7 @@ public class StateManager {
             vl.onVirusIncreased(increasedVirus);
     }
 
-    public synchronized static void updateAreaRecoveredNum() {
+    public static void updateAreaRecoveredNum() {
         List<Virus> recoverList = new ArrayList<>();
         for (int i = 0; i < 12; i++) {
             double rand = Math.random() / 2.0 + 0.75; // 0.75 ~ 1.25
